aa23bda3f5326747985e24ecd130e09c089fa2b4
João Victor Correa <correa.joaov25@gmail.com>
2024-09-06 09:30:27 -0300

    Firts Commit Backend

---
+++ ide-church-manager-backend/src/main/java/com/idechurch/manager/IdeChurchManagerApplication.java
@@ -0,0 +1,13 @@
+package com.idechurch.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeChurchManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeChurchManagerApplication.class, args);
+	}
+
+}
---
+++ ide-church-manager-backend/src/test/java/com/idechurch/manager/IdeChurchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idechurch.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeChurchManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
